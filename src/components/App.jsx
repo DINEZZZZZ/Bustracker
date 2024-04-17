5d5120774656ea254f960ea594224b3792fa63cb
=DINEZZZZZ <=dineshasus2023@gmail.com>
2024-04-18 01:40:08 +0530

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     <Router>
       
        <Routes>
-          <Route path="/" element={<QRScanner/>} />
+          <Route path="/Bustracker" element={<QRScanner/>} />
           </Routes>
        
       
